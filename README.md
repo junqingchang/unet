5f02aa1d431301521d75065d8476b4ad8f5b6422
junqingchang <jqchang95@gmail.com>
2020-11-09 20:04:17 +0800

    Add iou scoring

--- README.md
+++ README.md
@@ -1,18 +1,29 @@
 # UNet
 Reimplementation of UNet for Image Segmentation Course
 
+## Details on Project
+A review on UNet and how well it performs without any data augmentation (i.e. only how well the architecture will perform)
+
 ## Directory Structure
 ```
 chkpt/
-
+    fss1000-epoch100.pt
+    voc-epoch100.pt
 data/
     VOCdevkit/
         ...
 vocplots/
 
 .gitignore
+fss1000.py
+fssaccuracy.py
 README.md
 train.py
 unet.py
+visualise.py
 voc.py
+vocaccuracy.py
 ```
+
+## Models
+Pretrained models are available at https://drive.google.com/drive/folders/165oVabNzCnjuY0htpUxQK7JFU4H9nkIV?usp=sharing
---
+++ vocaccuracy.py
@@ -0,0 +1,59 @@
+import torch
+from torch.utils.data import DataLoader
+from voc import VOCSegmentation
+import numpy as np
+
+
+voc_checkpoint_dir = 'chkpt/voc-epoch100.pt'
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+num_classes = 21
+
+def intersect_and_union(pred, target, num_class):
+    '''
+    References https://github.com/chenxi116/DeepLabv3.pytorch/blob/master/utils.py
+    '''
+    pred = np.asarray(pred, dtype=np.uint8).copy()
+    target = np.asarray(target, dtype=np.uint8).copy()
+
+    # 255 -> 0
+    pred += 1
+    target += 1
+    pred = pred * (target > 0)
+
+    inter = pred * (pred == target)
+    (area_inter, _) = np.histogram(inter, bins=num_class, range=(1, num_class))
+    (area_pred, _) = np.histogram(pred, bins=num_class, range=(1, num_class))
+    (area_target, _) = np.histogram(target, bins=num_class, range=(1, num_class))
+    area_union = area_pred + area_target - area_inter
+
+    return (area_inter, area_union)
+
+def calculate_accuracy(loader, model, device):
+    model.eval()
+    total_intersect = np.array([0] * num_classes)
+    total_union = np.array([0] * num_classes)
+    with torch.no_grad():
+        for idx, (data, target) in enumerate(loader):
+            data = data.to(device)
+
+            output = model(data)
+            output_predictions = output.argmax(1).detach().cpu()
+            intersect, union = intersect_and_union(output_predictions, target, num_classes)
+            total_intersect += intersect
+            total_union += union
+            
+    iou = total_intersect/total_union
+    avg_iou = np.mean(iou)
+    return avg_iou
+
+
+
+if __name__ == '__main__':
+    model = torch.load(voc_checkpoint_dir)
+
+    val_data = VOCSegmentation('data/', image_set='val', h=None, w=None)
+    val_loader = DataLoader(val_data, batch_size=1, shuffle=False)
+
+    avg_iou = calculate_accuracy(val_loader, model, device)
+    print(f'IOU: {avg_iou*100}')
